a043de7c2f067d902002bc0075dbeabcc3fc4adb
mtmahe <mtmahe@gmail.com>
2021-04-03 13:51:01 -0300

    cleaned and added .gitignore

--- application.py
+++ application.py
@@ -26,20 +26,6 @@ toGetIex = [
     "CRM"
 ]
 
-# Make API call
-# response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{symbol}/quote?token={api_key}")
-# response.raise_for_status()
-
-# quote = response.json()
-
-# Clean up the response
-#data = {
-#    "name": quote["companyName"],
-#    "price": float(quote["latestPrice"]),
-#    "symbol": quote["symbol"],
-#    "volume": float(quote["iexVolume"])
-#}
-
 # Iterate through Iex list and make api calls
 responseDict = {}
 for item in toGetIex:
@@ -63,9 +49,6 @@ jsonData = json.dumps(responseDict)
 
 # Okay, now what does server need to do to break it up?
 req_data = json.loads(jsonData)
-#keys = newData.keys()
-#for key in keys:
-#    print(newData[key]["symbol"], newData[key]["date"])
 
 keys = req_data.keys()
 for key in keys:
@@ -76,10 +59,6 @@ for key in keys:
     date = req_data[key]["date"]
 
 print(type,name,symbol,price,date)
-
-
-# Convert to json
-# jsonData = json.dumps(data)
 
 # Post it
 newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
